b5830bf3acb849f1a5dcfe5296abfc49a6d5b459
Enous <scorpiusanna@gmail.com>
2025-08-26 18:39:35 +0300

    Add getComputerChoice() function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello, World");
+const ROCK_PAPER_SCISSORS = ["Rock", "Paper", "Scissors"];
+
+
+function getComputerChoice()
+{
+    return ROCK_PAPER_SCISSORS[Math.floor(Math.random() * 3)];
+}
+
+
+console.log(getComputerChoice());
